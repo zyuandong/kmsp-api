5d5ddbaa6265b7c769b9175f27fc67553b656a70
zyuandong <de1719@163.com>
2021-09-07 16:45:41 +0800

    [modules] add: axios & update graphql schema

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,61 +1,44 @@
-const router = require('koa-router')()
-const koaGraphql = require('koa-graphql')
-const userSchema = require('../schemas/user')
-const spacexSchema = require('../schemas/spaceX')
+const router = require('koa-router')();
+const koaGraphql = require('koa-graphql');
+const userSchema = require('../schemas/user');
+const {query} = require('../schemas/spaceX');
 // const http = require('http')
-const https = require('https')
+const https = require('https');
 
 const resolvers = {
-  hello: () => `hello world`
-}
+  hello: () => `hello world`,
+};
 
-router.all('/graphql', koaGraphql({
-  schema: userSchema,
-  rootValue: resolvers,
-  graphiql: true
-}))
+router.all(
+  '/graphql',
+  koaGraphql({
+    schema: userSchema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
 
-// const spacex = {
-//   launches: https.get('https://api.spacexdata.com/v3/launches', (res) => {
-//     // console.log('statusCode: ', res.statusCode);
-//     // console.log('headers: ', res.headers);
-
+// const spacexData = async () => {
+//   await https.get('https://api.spacexdata.com/v3/launches', (res) => {
 //     let str = '';
 //     res.on('data', (chunk) => {
 //       str += chunk;
-//     })
+//     });
 
 //     res.on('end', () => {
-//       // console.log(JSON.parse(str)[0]);
-//       return JSON.parse(str)[0]
-//     })
-//   })
-// }
+//       console.log(JSON.parse(str)[0]);
+//       return JSON.parse(str)[0];
+//       // return
+//     });
+//   });
+// };
 
-const spacexData = () => {
-  https.get('https://api.spacexdata.com/v3/launches', (res) => {
-    let str = ''
-    res.on('data', (chunk) => {
-      str += chunk
-    })
-
-    res.on('end', () => {
-      // console.log(JSON.parse(str)[0]);
-      return JSON.parse(str)[0];
-      // return 
-    })
+router.all(
+  '/graphql/space_x',
+  koaGraphql({
+    schema: query,
+    graphiql: true,
   })
-}
-
-router.all('/graphql/space_x', koaGraphql({
-  schema: spacexSchema,
-  rootValue: spacexData(),
-  // rootValue: {
-  //   flight_number: 1,
-  //   mission_name: 'FalconSat',
-  //   upcoming: false
-  // },
-  graphiql: true
-}))
+);
 
 module.exports = router;
--- schemas/spaceX.js
+++ schemas/spaceX.js
@@ -1,14 +1,116 @@
-const buildSchema = require('graphql').buildSchema;
+const {
+  buildSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+} = require('graphql');
+// const https = require('https')
+const axios = require('axios');
 
-const schema = buildSchema(`
-  type Query {
-    # launches: [Rocket]
-    mission_name: String
-  }
-  # type Rocket {
-    # flight_number: Int,
-    # mission_name: String
-  # }
-`)
+// const schema = buildSchema(`
+//   type Query {
+//     mission_name: String
+//     flight_number: Int,
+//     upcoming: Boolean
+//   }
+// `);
 
-module.exports = schema;
+const missionIdType = new GraphQLList(GraphQLString);
+
+const rocketType = new GraphQLObjectType({
+  name: 'rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+    first_stage: {
+      type: new GraphQLObjectType({
+        name: 'firstStage',
+        fields: () => ({
+          cores: {
+            type: new GraphQLList(
+              new GraphQLObjectType({
+                name: 'core',
+                fields: () => ({
+                  core_serial: { type: GraphQLString },
+                }),
+              })
+            ),
+          },
+        }),
+      }),
+    },
+    second_stage: {
+      type: new GraphQLObjectType({
+        name: 'scondStage',
+        fields: () => ({
+          block: { type: GraphQLInt },
+        }),
+      }),
+    },
+  }),
+});
+
+const launchType = new GraphQLObjectType({
+  name: 'launch',
+  fields: {
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    mission_id: { type: missionIdType },
+    upcoming: { type: GraphQLBoolean },
+    launch_year: { type: GraphQLString },
+    launch_date_unix: { type: GraphQLInt },
+    launch_date_utc: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    is_tentative: { type: GraphQLBoolean },
+    tentative_max_precision: { type: GraphQLString },
+    tbd: { type: GraphQLBoolean },
+    launch_window: { type: GraphQLInt },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: rocketType },
+  },
+});
+
+const rootQueryType = new GraphQLObjectType({
+  name: 'rootQuery',
+  fields: {
+    launches: {
+      type: new GraphQLList(launchType),
+      args: {
+        page: { type: GraphQLInt },
+        pageSize: { type: GraphQLInt },
+      },
+      async resolve(parent, { page, pageSize }) {
+        return await axios.get(`https://api.spacexdata.com/v3/launches`).then((res) => {
+          if (page && pageSize) {
+            const start = (page - 1) * pageSize;
+            const end = start + pageSize;
+            return res.data.slice(start, end);
+          } else {
+            return res.data;
+          }
+        });
+      },
+    },
+    launch: {
+      type: launchType,
+      args: {
+        flight_number: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+const query = new GraphQLSchema({
+  query: rootQueryType,
+});
+
+module.exports = { query };
